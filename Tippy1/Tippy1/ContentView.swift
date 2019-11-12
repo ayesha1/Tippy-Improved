012d8ad8cb99187054a5bfb0c6a9eb4b9e0db3d7
Ayesha Khan <akhan1231@hotmail.com>
2019-11-12 11:41:38 -0800

    UI Fixes, added app Icon, Launch Screen update

--- Tippy1/Tippy1/ContentView.swift
+++ Tippy1/Tippy1/ContentView.swift
@@ -10,20 +10,41 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var tip = 0.0
-    @State public var total = 0.0
+    @State private var total = 0.0
+    @State private var amount = ""
 
     var body: some View {
-        VStack {
-            HStack {
-                Text("Bill Amount")
-                TextField("Enter Bill Amount", text: .constant("")).textFieldStyle(RoundedBorderTextFieldStyle())
-            }
-            Text("Tip %")
-            Slider(value: $tip, in: 0...25)
-            Text("\(Int(tip))")
-            HStack {
-                Text("Total")
-                Text("$\(total)")
+        ZStack {
+            Image(uiImage: #imageLiteral(resourceName: "coins (2)"))
+                .padding(.horizontal, 20.0)
+                .frame(width: 40.0, height: 40.0)
+            VStack {
+                HStack {
+                    Text("Bill Amount")
+                        .font(.headline)
+                        .fontWeight(.regular)
+                        
+                        
+                    TextField("Enter Bill Amount", text: $amount).padding().textFieldStyle(RoundedBorderTextFieldStyle()).keyboardType(.decimalPad)
+                }
+                .padding(.horizontal, 13.0)
+                Text("Tip %")
+                    .font(.headline)
+                    .fontWeight(.regular)
+                Slider(value: $tip, in: 0...25)
+                    .padding(.horizontal, 13.0)
+                if tip != 0 {
+                    Text("\(Int(tip))")
+                }
+                HStack {
+                    Text("Total")
+                        .font(.largeTitle)
+                        .fontWeight(.light)
+                    Text("$\(String(format: "%.2f", (Double(amount) ?? 0.0) + (Double(amount) ?? 0.0)*(Double(Int(tip)) * 0.01)))")
+                        .font(.largeTitle)
+                        .fontWeight(.thin)
+                }
+                .padding(.top, 20.0)
             }
         }
     }
@@ -34,3 +55,5 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+// <div>Icons made by <a href="https://www.flaticon.com/authors/smashicons" title="Smashicons">Smashicons</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
